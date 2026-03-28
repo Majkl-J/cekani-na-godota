b5b687145b54d210a1bb18577272760f78336cbf
Ivan Metelka <met0015@slu.cz>
2026-03-28 23:01:23 +0100

    nápověda kytky

--- mobs/kytka.gd
+++ mobs/kytka.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var puzzle = $"../FlowerPuzzle"
+@onready var hint_sprite: Sprite2D = $HintSprite
 
 @export var flower_id: int = 1
 
@@ -9,15 +10,20 @@ var player_in_range = false
 var busy = false
 
 func _ready():
+	print(get_node_or_null("HintSprite"))
+	print(get_node_or_null("Sprite2D"))
 	animated_sprite_2d.play("default")
+	hint_sprite.visible = false
 
 func _on_area_2d_body_entered(body):
 	if body.is_in_group("player"):
 		player_in_range = true
+		hint_sprite.visible = true
 
 func _on_area_2d_body_exited(body):
 	if body.is_in_group("player"):
 		player_in_range = false
+		hint_sprite.visible = false
 
 func _process(_delta):
 	if busy:

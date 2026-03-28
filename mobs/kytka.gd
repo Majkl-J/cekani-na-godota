d9907b7280ceacd27291dbc8ef9d8988af33d73f
JachymMetelka <jachym.metelka@gmail.com>
2026-03-28 17:35:51 +0100

    kvitka

--- mobs/kytka.gd
+++ mobs/kytka.gd
@@ -1,50 +1,51 @@
 extends Node2D
 
-@onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var puzzle = $"../FlowerPuzzle"
 
 @export var flower_id: int = 1
 
 var player_in_range = false
+var busy = false
 
 func _ready():
 	animated_sprite_2d.play("default")
-	print("kytka ready:", name, " id:", flower_id)
 
 func _on_area_2d_body_entered(body):
-	print("body entered:", body.name)
 	if body.is_in_group("player"):
 		player_in_range = true
-		print("player in range:", flower_id)
 
 func _on_area_2d_body_exited(body):
-	print("body exited:", body.name)
 	if body.is_in_group("player"):
 		player_in_range = false
-		print("player out of range:", flower_id)
-
-func _on_area_2d_area_entered(area):
-	print("area entered:", area.name)
-	if area.is_in_group("player"):
-		player_in_range = true
-		print("player in range via area:", flower_id)
-
-func _on_area_2d_area_exited(area):
-	print("area exited:", area.name)
-	if area.is_in_group("player"):
-		player_in_range = false
-		print("player out of range via area:", flower_id)
 
 func _process(_delta):
-	if Input.is_action_just_pressed("interact"):
-		print("E u kytky", flower_id, "| in_range =", player_in_range)
+	if busy:
+		return
 
 	if player_in_range and Input.is_action_just_pressed("interact"):
-		print("zmacknuta kytka:", flower_id)
 		puzzle.flower_pressed(flower_id)
 
-func set_good():
+func play_correct():
+	if busy:
+		return
+
+	busy = true
+	print("spoustim good u kytky", flower_id)
 	animated_sprite_2d.play("good")
 
+	await animated_sprite_2d.animation_finished
+
+	print("good dokoncena u kytky", flower_id)
+
+	if animated_sprite_2d.sprite_frames.has_animation("open"):
+		animated_sprite_2d.play("open")
+		print("spoustim open u kytky", flower_id)
+	else:
+		print("chybi animace open u kytky", flower_id)
+
+	busy = false
+
 func set_default():
+	busy = false
 	animated_sprite_2d.play("default")
--- scenes/rooms/kytky.gd
+++ scenes/rooms/kytky.gd
@@ -7,20 +7,34 @@ extends Node
 
 var correct_order = [1, 3, 2, 4]
 var current_step = 0
+var puzzle_finished = false
 
-func flower_pressed(flower_id):
+func flower_pressed(flower_id: int):
+	if puzzle_finished:
+		print("puzzle uz hotovy")
+		return
+
+	if current_step >= correct_order.size():
+		puzzle_finished = true
+		print("puzzle uz hotovy")
+		return
+
 	if flower_id == correct_order[current_step]:
 		print("spravne")
-		get_flower_by_id(flower_id).set_good()
+		var flower = get_flower_by_id(flower_id)
+		if flower != null:
+			flower.play_correct()
 		current_step += 1
 
 		if current_step >= correct_order.size():
+			puzzle_finished = true
 			print("HOTOVO")
+			puzzle_done()
 	else:
 		print("spatne")
 		reset_puzzle()
 
-func get_flower_by_id(flower_id):
+func get_flower_by_id(flower_id: int):
 	match flower_id:
 		1: return kytka1
 		2: return kytka2
@@ -29,8 +43,14 @@ func get_flower_by_id(flower_id):
 	return null
 
 func reset_puzzle():
+	if puzzle_finished:
+		return
+
 	current_step = 0
 	kytka1.set_default()
 	kytka2.set_default()
 	kytka3.set_default()
 	kytka4.set_default()
+
+func puzzle_done():
+	print("PUZZLE SPLNEN")

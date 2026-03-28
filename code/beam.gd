70ceb8bedbefd0f12ea16fc4fac0c0b68ef511b6
Maia <maia@punches.cz>
2026-03-28 10:52:42 +0100

    Light beam demo- INCOMPLETE

--- code/beam.gd
+++ code/beam.gd
@@ -1,4 +1,65 @@
+class_name Beem
 extends Node2D
 
-func set_length(len: int):
+func kill_all_beems():
+	for beem in get_children():
+		remove_child(beem)
+		beem.free()
+
+var current_length: float = 270
+func set_current_length(new_length: float = 0):
+	set_length(new_length)
+	current_length = new_length
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventKey:
+		if event.pressed:
+			if Input.is_action_just_pressed("test_a"):
+				set_current_length(max(0, current_length - 10));
+			if Input.is_action_just_pressed("test_b"):
+				set_current_length(max(0, current_length + 10));
+
+# Look at this disgusting thing
+func set_length(set_len: float):
+	var beam_polys = get_children()
+	var default_beem: Resource = $"/root/controller".get_beem_resource()
+	var max_len: float = $"/root/controller".get_max_len()
+	if set_len <= 0:
+		kill_all_beems()
+		return
+	
+	# Populate beem
+	if len(beam_polys) <= 0:
+		var beem_x = 0
+		while(set_len >= max_len):
+			summon_beem(default_beem, max_len, beem_x)
+			beem_x += max_len
+			set_len -= max_len
+		summon_beem(default_beem, set_len, beem_x)
+		return
+	# Adjust beem
+	var beem_count = ceil(set_len / max_len)
+	# :33333333333333 This is trash
+	for i in range(max(beem_count, len(beam_polys))):
+		var b: BeemPoly = get_child(i)
+		if(b == null && i <= beem_count):
+			if(i+1 == beem_count):
+				summon_beem(default_beem, fmod(set_len, max_len), max_len * i+1)
+			else:
+				summon_beem(default_beem, max_len, max_len * i+1)
+			continue
+		if(i+1 > beem_count):
+			remove_child(b)
+			b.queue_free()
+			continue
+		if(i+1 == beem_count && fmod(set_len, max_len) < max_len):
+			b.set_length(fmod(set_len, max_len))
+			continue
+		b.set_length(max_len)
 	return
+
+func summon_beem(beem_res: Resource, beem_len: float, x_shift: float = 0):
+	var beem: BeemPoly = beem_res.instantiate()
+	add_child(beem)
+	beem.set_length(beem_len)
+	beem.position.x = x_shift
--- code/polygon_2d.gd
+++ code/polygon_2d.gd
@@ -1,19 +1,16 @@
+class_name BeemPoly
+
 extends Polygon2D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	uv_multiplier = uv[2].x / polygon[2].x
-	max_len = polygon[2].x
 	pass # Replace with function body.
 
 static var uv_multiplier = 1
-static var max_len = 200
 
-func get_max_len():
-	return max_len
-
-func new_length(len: float):
+func set_length(len: float):
 	polygon[2].x = len
 	polygon[3].x = len
 	uv[2].x = len * uv_multiplier

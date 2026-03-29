fdd6e63de469c0b7d14be6beb49f29fa444bf8a4
Ivan Metelka <met0015@slu.cz>
2026-03-29 10:14:10 +0200

    music

--- code/controller.gd
+++ code/controller.gd
@@ -43,7 +43,17 @@ func get_max_len():
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	load_level(first_scene)
-	pass # Replace with function body.
+	
+	player = AudioStreamPlayer.new()
+	add_child(player)
+	player.bus = "music"
+	player.volume_db = -8.0
+	player.finished.connect(_on_track_finished)
+
+	if playlist.size() > 0:
+		player.stream = playlist[current_track_index]
+		player.play()
+	#pass # Replace with function body.
 
 func load_actual_game():
 	load_level(first_level)
@@ -122,3 +132,51 @@ func get_walk_door_id():
 	return walk_door_id
 
 static var flowers_complete: bool = false
+
+var player: AudioStreamPlayer
+
+var playlist: Array[AudioStream] = [
+	preload("res://sounds/numbthefeelings - glass house (freetouse.com).mp3"),
+	preload("res://sounds/Pufino - Arbiters Trial (freetouse.com) (1).mp3"),
+	preload("res://sounds/Zambolino - The Living Dead (freetouse.com).mp3")
+]
+
+var current_track_index: int = 0
+
+# Music
+func _on_track_finished() -> void:
+	if playlist.is_empty():
+		return
+
+	current_track_index = (current_track_index + 1) % playlist.size()
+	player.stream = playlist[current_track_index]
+	player.play()
+
+func play_playlist() -> void:
+	if playlist.is_empty():
+		return
+
+	if not player.playing:
+		player.stream = playlist[current_track_index]
+		player.play()
+
+func stop_music() -> void:
+	player.stop()
+
+func next_track() -> void:
+	if playlist.is_empty():
+		return
+
+	current_track_index = (current_track_index + 1) % playlist.size()
+	player.stop()
+	player.stream = playlist[current_track_index]
+	player.play()
+
+func previous_track() -> void:
+	if playlist.is_empty():
+		return
+
+	current_track_index = (current_track_index - 1 + playlist.size()) % playlist.size()
+	player.stop()
+	player.stream = playlist[current_track_index]
+	player.play()
